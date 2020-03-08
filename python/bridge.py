cba16a93430d95805b1dcc1ba7cab9f5705378d4
Jay <jmandav1@jhu.edu>
2020-03-08 11:04:57 -0400

    copied some phidget code

---
+++ python/bridge.py
@@ -0,0 +1,22 @@
+from Phidget22.Phidget import *
+from Phidget22.Devices.VoltageRatioInput import *
+import time
+
+def onVoltageRatioChange(self, voltageRatio):
+    print("VoltageRatio: " + str(voltageRatio))
+
+    def main():
+        voltageRatioInput0 = VoltageRatioInput()
+
+        voltageRatioInput0.setOnVoltageRatioChangeHandler(onVoltageRatioChange)
+
+        voltageRatioInput0.openWaitForAttachment(5000)
+
+        try:
+        input("Press Enter to Stop\n")
+        except (Exception, KeyboardInterrupt):
+        pass
+
+    voltageRatioInput0.close()
+
+    main()
